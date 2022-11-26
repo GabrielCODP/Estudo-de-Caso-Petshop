713b4b4545d5379d8d8fb1baacf70eb768777bbd
Ana Lívia Louise Silva <49314087+AnaLiviaLLS@users.noreply.github.com>
2022-11-25 22:07:10 -0300

    Add files via upload
    CepsFuncClienteTelefone

---
+++ Application/Controllers/FuncionarioController.cs
@@ -0,0 +1,144 @@
+﻿using ApiDomain.Dtos.Funcionario;
+using ApiDomain.Interfaces.Services.Funcao;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FuncionarioController : ControllerBase
+    {
+        public IFuncionarioService _service { get; set; }
+        public FuncionarioController(IFuncionarioService service)
+        {
+            _service = service;
+        }
+
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("{id}", Name = "GetFuncionarioWithId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Get(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+
+        }
+
+        [Authorize("Bearer")]
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] FuncionarioDtoCreate dtoCreate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(dtoCreate);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetFuncionarioWithId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] FuncionarioDtoUpdate dtoUpdate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(dtoUpdate);
+                if (result == null)
+                {
+                    return BadRequest();
+                }
+
+                return Ok(result);
+
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+
+        }
+
+        [Authorize("Bearer")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+    }
+}
